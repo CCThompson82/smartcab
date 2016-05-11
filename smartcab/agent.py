93fedf72fe546a1f8f02300e68bd6fecdf5d6412
Christopher Thompson <christophercthompson82@gmail.com>
2016-05-11 19:05:21 +0100

    Attempt to initialize reward during agent init

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -10,7 +10,7 @@ class LearningAgent(Agent):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
-        #self.state = (("R(s)", None),("light",None), ("oncoming", None), ("left",None)
+        reward = None
         # TODO: Initialize any additional variables here
         #self.q_table = {}
 
@@ -26,7 +26,7 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        self.state = (("R(s)", None),("light",inputs['light']), ("oncoming", inputs['oncoming']), ("left",inputs['left']))
+        self.state = (("R(s)", reward),("light",inputs['light']), ("oncoming", inputs['oncoming']), ("left",inputs['left']))
 
         # TODO: Select action according to your policy
         action = random.choice([None, 'forward', 'left', 'right'])
